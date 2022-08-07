7beb39b56aaed30c38fc2ceda621d296eb9f21ee
ShinDongJun <wq0212@naver.com>
2022-08-07 16:41:24 +0900

    add firebase fcm

--- App.tsx
+++ App.tsx
@@ -17,9 +17,40 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import messaging from '@react-native-firebase/messaging';
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+}
 const App = () => {
+  
+  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+    console.log('Message handled in the background!', remoteMessage);
+  });
+  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+    //  여기에 로직을 작성한다.
+    //  remoteMessage.data로 메세지에 접근가능
+    //  remoteMessage.from 으로 topic name 또는 message identifier
+    //  remoteMessage.messageId 는 메시지 고유값 id
+    //  remoteMessage.notification 메시지와 함께 보내진 추가 데이터
+    //  remoteMessage.sentTime 보낸시간
+    });
+    
+    
+    // Foreground 상태인 경우
+    React.useEffect(() => {
+      const unsubscribe = messaging().onMessage(async remoteMessage => {
+        Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      });
+      return unsubscribe;
+    });
+
   useEffect(() => {
     SplashScreen.hide();
   }, []);
