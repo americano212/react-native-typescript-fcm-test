775cc0fb347f30afae8a06d3b7d0107af76e63da
ShinDongJun <wq0212@naver.com>
2022-09-27 17:22:43 +0900

    feat: update App.tsx

--- App.tsx
+++ App.tsx
@@ -29,30 +29,30 @@ async function requestUserPermission() {
   }
 }
 const App = () => {
-  
-  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
     console.log('Message handled in the background!', remoteMessage);
   });
-  messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
     //  여기에 로직을 작성한다.
     //  remoteMessage.data로 메세지에 접근가능
     //  remoteMessage.from 으로 topic name 또는 message identifier
     //  remoteMessage.messageId 는 메시지 고유값 id
     //  remoteMessage.notification 메시지와 함께 보내진 추가 데이터
     //  remoteMessage.sentTime 보낸시간
-    });
-    
-    
-    // Foreground 상태인 경우
-    React.useEffect(() => {
-      const unsubscribe = messaging().onMessage(async remoteMessage => {
-        Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-      });
-      return unsubscribe;
-    });
+  });
 
+  // Foreground 상태인 경우
+  // React.useEffect(() => {
+  //   const unsubscribe = messaging().onMessage(async remoteMessage => {
+  //     Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+  //   });
+  //   return unsubscribe;
+  // });
   useEffect(() => {
-    SplashScreen.hide();
+    setTimeout(() => {
+      console.log('????');
+      SplashScreen.hide();
+    }, 2000);
   }, []);
   const isDarkMode = useColorScheme() === 'dark';
 
--- ios/myProject/AppDelegate.mm
+++ ios/myProject/AppDelegate.mm
@@ -60,6 +60,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
